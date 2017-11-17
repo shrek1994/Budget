1b08449f8ccb3fedec927db30cf288d47d8a255f
shrek1994 <maciej.wozny41@gmail.com>
2017-11-18 00:28:19 +0100

    added addition expense

--- app/src/main/java/com/maciejwozny/budget/BudgetActivity.java
+++ app/src/main/java/com/maciejwozny/budget/BudgetActivity.java
@@ -1,5 +1,6 @@
 package com.maciejwozny.budget;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class BudgetActivity extends AppCompatActivity {
+    Calendar myCalendar = Calendar.getInstance();
+    EditText expenseDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,40 @@ public class BudgetActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        expenseDate = (EditText) findViewById(R.id.expenceDateEditText);
+
+//*********************************************************************************************//
+        //TODO extract to method or class
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        expenseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(BudgetActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+//*********************************************************************************************//
+    }
+
+    private void updateLabel() {
+        String myFormat = "yyyy-MM-dd"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        expenseDate.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
